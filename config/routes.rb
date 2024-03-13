6c5decf5790ac506635b471a1d5de01451ddd78f
ntienthang <thangnguyen2@teqnological.asia>
2024-03-13 10:55:54 +0700

    init app

---
+++ config/routes.rb
@@ -0,0 +1,26 @@
+Rails.application.routes.draw do
+  get 'admin' => 'admin#index'
+  controller :sessions do
+    get  'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+  get 'sessions/create'
+  get 'sessions/destroy'
+
+
+  # START_HIGLIGHT
+  resources :support_requests, only: %i[ index update ]
+  # END_HIGLIGHT
+  resources :users
+  resources :products do
+    get :who_bought, on: :member
+  end
+
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store_index', via: :all
+  end
+end
